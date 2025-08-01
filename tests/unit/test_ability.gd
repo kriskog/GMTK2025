5678efa50d291f307f7f534ec2cb7493fc9df5db
Thomas Harrison <tmrharrison@gmail.com>
2025-08-01 01:23:09 -0400

    make abilities and visible character nodes and test them

---
+++ tests/unit/test_ability.gd
@@ -0,0 +1,28 @@
+extends GutTest
+
+
+func before_each():
+	gut.p("ran setup", 2)
+
+
+func after_each():
+	gut.p("ran teardown", 2)
+
+
+func before_all():
+	gut.p("ran run setup", 2)
+
+
+func after_all():
+	gut.p("ran run teardown", 2)
+
+
+func test_ability_calc_damage():
+	# Calculate the damage of an ability with 2 base damage and a bonus of 3
+	var ability: Ability = autofree(Ability.new())
+	ability.base_damage = 2
+
+	assert_eq(
+		ability.get_total_damage(3),
+		5
+	)
