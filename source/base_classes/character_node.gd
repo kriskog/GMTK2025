b7978a1e7ae473095952b76f787c3ab704d03814
Thomas Harrison <tmrharrison@gmail.com>
2025-08-03 09:32:08 -0400

    fix combat menu

--- source/base_classes/character_node.gd
+++ source/base_classes/character_node.gd
@@ -24,10 +24,9 @@ var _turn_starting: bool = false
 
 #region OnReadyVars
 @onready var combat_menu: CombatMenu = $CombatMenu
-#@onready var _blood_particles: CPUParticles2D = $blood_particles
-@onready var _health_bar: ProgressBar = $HealthBar
-@onready var _mana_bar: ProgressBar = $ManaBar
 @onready var animation: AnimationPlayer = $animation
+@onready var _health_bar: ProgressBar = $sprite/HealthBar
+@onready var _mana_bar: ProgressBar = $sprite/ManaBar
 @onready var _blood_particles: CPUParticles2D = $sprite/blood_particles
 #endregion
 
@@ -35,20 +34,24 @@ var _turn_starting: bool = false
 #region BuiltinMethods
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	combat_menu.character = self
-	_health_bar.max_value = get_stat_total(Global.Stats.MAX_HEALTH)
-	_health_bar.value = get_stat_total(Global.Stats.HEALTH)
-	_mana_bar.max_value = get_stat_total(Global.Stats.MAX_MANA)
-	_mana_bar.value = get_stat_total(Global.Stats.MANA)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	_mana_bar.value = get_stat_total(Global.Stats.MANA)
 	if not Engine.is_editor_hint():
 		combat_menu.character = self
 		combat_menu.hide()
 		$sprite.texture = texture
+
+		_health_bar.max_value = get_stat_total(Global.Stats.MAX_HEALTH)
+		_health_bar.value = get_stat_total(Global.Stats.HEALTH)
+		_mana_bar.max_value = get_stat_total(Global.Stats.MAX_MANA)
+		_mana_bar.value = get_stat_total(Global.Stats.MANA)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		if texture != null:
+			$sprite.texture = texture
+		else:
+			$sprite.texture = load("res://assets/sprites/Paladin.png")
 
 
 #endregion
@@ -57,6 +60,7 @@ func _process(_delta: float) -> void:
 #region PublicMethods
 func take_damage(val: int, ignore_defend: bool = false) -> int:
 	var total_damage = super.take_damage(val, ignore_defend)
+	_health_bar.value = get_stat_total(Global.Stats.HEALTH)
 	self.add_child(DamageNumber.new(total_damage))
 	if total_damage > 0:
 		_blood_particles.restart()
@@ -69,6 +73,10 @@ func handle_turn() -> void:
 	_turn_starting = true
 
 
+func spend_mana(val: int) -> bool:
+	var ret_val = super.spend_mana(val)
+	_mana_bar.value = get_stat_total(Global.Stats.MANA)
+	return ret_val
 #endregion
 
 #region PrivateMethods
