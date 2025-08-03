f965455eac71b2de4174fc5b06b28f66289a770d
David Beauchamp <davidcbeauchamp@hotmail.com>
2025-08-03 11:22:40 -0500

    victory screen added and fonts increased

--- source/scenes/ui/menus/vic_screen.gd
+++ source/scenes/ui/menus/vic_screen.gd
@@ -3,11 +3,11 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$VBoxContainer/RichTextLabel.text = "[center]Attempts: " + str(Global.current_loop)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
